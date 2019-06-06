8e62e17e7c70329728ac5c1418db8167a6b055fb
Euclidr <euclidry@gmail.com>
2019-06-06 17:25:17 +0800

    extract yield curve data

--- yield_curve/src/main.rs
+++ yield_curve/src/main.rs
@@ -6,12 +6,18 @@ extern crate serde_json;
 
 use futures::{future, Future, Stream};
 use std::collections::HashMap;
+use std::convert::From;
+use std::error;
+use std::error::Error;
+use std::fmt;
 
+use hyper::client::HttpConnector;
 use hyper::service::service_fn;
 use hyper::{Body, Client, Method, Request, Response, Server, StatusCode};
 use hyper_tls::HttpsConnector;
 
 use select::document::Document;
+use select::node::Node;
 use select::predicate::{Class, Name, Predicate};
 
 static NOTFOUND: &[u8] = b"Not Found";
@@ -20,59 +26,130 @@ static BOND_URL: &str = "https://www.treasury.gov/resource-center/data-chart-cen
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type ResponseFuture = Box<Future<Item = Response<Body>, Error = GenericError> + Send>;
+type HttpsClient = Client<HttpsConnector<HttpConnector>>;
 
-fn handle_bond_yield(_: Request<Body>) -> ResponseFuture {
-    let https = HttpsConnector::new(4).unwrap();
-    let client = Client::builder().build::<_, hyper::Body>(https);
+#[derive(Debug, Clone)]
+struct XError {
+    msg: String,
+}
+
+impl fmt::Display for XError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl error::Error for XError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+    fn description(&self) -> &str {
+        &self.msg
+    }
+}
+
+fn extract_table_keys(row: &Node) -> Vec<String> {
+    let mut keys = vec![];
+    for (_, th) in row.find(Name("th")).enumerate() {
+        keys.push(th.text())
+    }
+    keys
+}
+
+fn extract_row(row: &Node, keys: &Vec<String>) -> Option<HashMap<String, String>> {
+    if keys.len() == 0 {
+        return None;
+    }
+
+    let cnt = row.find(Name("td")).count();
+    if cnt != keys.len() {
+        return None;
+    }
+
+    let mut record = HashMap::new();
+    for (i, td) in row.find(Name("td")).enumerate() {
+       record.insert(keys[i].clone(), td.text());
+    }
+
+    Some(record)
+} 
+
+fn extract_yield_data(text: &str) -> Result<String, GenericError> {
+    let document = Document::from(&text[..]);
+    let mut tcharts_result = document.find(Class("t-chart"));
+    let table = tcharts_result.next();
+    let data = match table {
+        Some(table) => {
+            let trs = table.find(Name("tr"));
+            let mut keys = vec![];
+            let mut data = vec![];
+            for (i, row) in trs.enumerate() {
+                if i == 0 {
+                    keys = extract_table_keys(&row);
+                } else {
+                    match extract_row(&row, &keys) {
+                        Some(record) => data.push(record),
+                        None => continue,
+                    }
+                }
+            }
+            data
+        }
+        None => return Err(From::from(XError{msg: "table notfound".to_string()})),
+    };
+
+    let data_str = serde_json::to_string(&data)?;
+    Ok(data_str)
+}
+
+fn handle_bond_yield(_: Request<Body>, client: &HttpsClient) -> ResponseFuture {
     let uri = BOND_URL.parse().unwrap();
     let r = client
         .get(uri)
-        .map_err(|e| {
-            println!("{}", e);
-            e
+        .map_err(|e| XError { msg: e.to_string() })
+        .and_then(|resp| {
+            if !resp.status().is_success() {
+                return future::Either::A(future::err(XError {
+                    msg: "Invalid code".to_string(),
+                }));
+            };
+            future::Either::B(
+                resp.into_body()
+                    .concat2()
+                    .map_err(|e| XError { msg: e.to_string() })
+                    .map(|chunk| String::from(std::str::from_utf8(&chunk).unwrap())),
+            )
         })
-        .from_err()
-        .and_then(|res| {
-            res.into_body()
-                .concat2()
-                .map_err(|err| {
-                    println!("{}", err);
-                    err
-                })
-                .map(|chunk| {
-                    // println!("dddhsdslsdfs----{}", std::str::from_utf8(&chunk));
-                    // TODO handle error
-                    String::from(std::str::from_utf8(&chunk).unwrap())
-                })
-                .from_err()
-        })
-        .map(|text| {
-            let document = Document::from(&text[..]);
-            let mut tcharts_result = document.find(Class("t-chart"));
-            let table = tcharts_result.next();
-            let mut cates = HashMap::new();
-            match table {
-                Some(table) => {
-                    for th in table.find(Name("th")) {
-                        cates.insert(th.text(), 1);
-                    }
-                }
-                None => (),
+        .and_then(|text| {
+            match extract_yield_data(&text) {
+                Ok(data_str) => future::ok(data_str),
+                Err(err) => future::err(XError {
+                    msg: err.to_string()
+                }),
             }
-            let fin = serde_json::to_string(&cates).unwrap();
-            Response::new(Body::from(fin))
-        });
+        })
+        .then(|result| {
+            match result {
+                Ok(data) => future::ok(Response::new(Body::from(data))),
+                Err(e) => future::ok(Response::new(Body::from(e.to_string()))),
+                // #[warn(unreachable_patterns)]
+                _ => future::err(XError {
+                    msg: "will not happen".to_string(),
+                }),
+            }
+        })
+        .from_err();
 
     Box::new(r)
 }
 
-fn route(req: Request<Body>) -> ResponseFuture {
+fn route(req: Request<Body>, client: &HttpsClient) -> ResponseFuture {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") | (&Method::GET, "/index.html") => {
             let body = Body::from(INDEX);
             Box::new(future::ok(Response::new(body)))
         }
-        (&Method::GET, "/bond_yield") => handle_bond_yield(req),
+        (&Method::GET, "/bond_yield") => handle_bond_yield(req, client),
         _ => {
             let body = Body::from(NOTFOUND);
             Box::new(future::ok(
@@ -89,7 +166,13 @@ fn main() {
     let addr = "127.0.0.1:2008".parse().unwrap();
 
     hyper::rt::run(future::lazy(move || {
-        let new_service = move || service_fn(move |req| route(req));
+        let https = HttpsConnector::new(4).unwrap();
+        let client = Client::builder().build::<_, hyper::Body>(https);
+
+        let new_service = move || {
+            let client = client.clone();
+            service_fn(move |req| route(req, &client))
+        };
 
         let server = Server::bind(&addr)
             .serve(new_service)
