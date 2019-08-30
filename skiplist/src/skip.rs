ddc7030d22cef74c460c47cbee5f9506e4b446c0
Euclidr <euclidry@gmail.com>
2019-08-30 18:06:02 +0800

    add remove

--- skiplist/src/skip.rs
+++ skiplist/src/skip.rs
@@ -94,6 +94,12 @@ impl<K: Ord, V> SkipList<K, V> {
         let mut result = None;
 
         if pre.is_null() {
+            if is_equal{
+                let mut old = self.next.take().unwrap();
+                result = Some(old.value);
+                new_node.next = old.next.take();
+                self.next = Some(new_node);
+            } else {
                 self.next = match self.next.take() {
                     None => Some(new_node),
                     Some(old_node) => {
@@ -101,6 +107,7 @@ impl<K: Ord, V> SkipList<K, V> {
                         Some(new_node)
                     }
                 }
+            }
         } else {
             let pre_node = unsafe {
                 &mut *pre
@@ -118,17 +125,64 @@ impl<K: Ord, V> SkipList<K, V> {
         result
     }
 
-    fn get(&self, q :&K) -> Option<&V> {
-        let p_result = self.get_node(q);
-
-        if !p_result.is_null() {
-            let result_node = unsafe {
-                &*p_result
-            };
-            Some(&result_node.value)
-        } else {
-            None
+    fn remove(&mut self, q :&K) -> Option<V> {
+        if self.nexts.len() == 0 {
+            return None;
         }
+        let mut level = self.nexts.len() - 1;
+        let mut nexts = &mut self.nexts;
+        let mut pre = std::ptr::null_mut();
+        let mut cur = std::ptr::null_mut();
+        let mut is_equal = false;
+        loop {
+            if !nexts[level].is_null() {
+                let tmp_key = unsafe {
+                    &(*nexts[level]).key
+                };
+                match q.cmp(tmp_key) {
+                    Ordering::Greater => {
+                        cur = nexts[level];
+                        nexts = unsafe {
+                            &mut (*nexts[level]).nexts
+                        };
+                        continue;
+                    },
+                    Ordering::Equal => {
+                        pre = cur;
+                        nexts[level] = unsafe {
+                            (*nexts[level]).nexts[level]
+                        };
+                        is_equal = true;
+                    },
+                    Ordering::Less => (),
+                }
+            }
+            if level == 0 {
+                break;
+            }
+            level -= 1;
+        }
+
+        if !is_equal {
+            return None
+        }
+
+        let result;
+
+        if pre.is_null() {
+            let mut toremove = self.next.take().unwrap();
+            self.next = toremove.next.take();
+            result = Some(toremove.value);
+        } else {
+            let pre_node = unsafe {
+                &mut *pre
+            };
+            let mut cur = pre_node.next.take().unwrap();
+            pre_node.next = cur.next.take();
+            result = Some(cur.value);
+        }
+
+        result
     }
 
     fn get_node(&self, q :&K) -> *mut Node<K, V> {
@@ -163,6 +217,20 @@ impl<K: Ord, V> SkipList<K, V> {
         p_result
     }
 
+    fn get(&self, q :&K) -> Option<&V> {
+        let p_result = self.get_node(q);
+
+        if !p_result.is_null() {
+            let result_node = unsafe {
+                &*p_result
+            };
+            Some(&result_node.value)
+        } else {
+            None
+        }
+    }
+
+
     fn get_mut(&mut self, q :&K) -> Option<&mut V> {
         let p_result = self.get_node(q);
 
@@ -188,6 +256,7 @@ mod tests {
         sk.insert("ab".to_string(), "ab1".to_string());
         sk.insert("dd".to_string(), "dd1".to_string());
         sk.insert("aa".to_string(), "aa2".to_string());
+        sk.insert("a".to_string(), "a1".to_string());
 
         let aa = "aa".to_string();
         let ab = "ab".to_string();
@@ -202,11 +271,57 @@ mod tests {
         println!("dd: {:?}", sk.get_mut(&dd));
         println!("cc: {:?}", sk.get_mut(&cc));
 
+        let r_ab = sk.remove(&ab);
+        println!("r_ab: {:?}", r_ab);
+        let r_ab = sk.remove(&ab);
+        println!("r_ab: {:?}", r_ab);
+        let r_dd = sk.remove(&dd);
+        println!("r_dd: {:?}", r_dd);
+        println!("dd: {:?}", sk.get(&dd));
+
         match sk.get_mut(&dd) {
             None => (),
             Some(ddv) => *ddv = "dd2".to_string(),
         };
 
+        let a = sk.get_mut(&dd);
+        match a {
+            None => (),
+            Some(ddv) => *ddv = "dd3".to_string(),
+        };
+
+        let b = sk.get_mut(&dd);
+        match b {
+            None => (),
+            Some(ddv) => *ddv = "dd4".to_string(),
+        };
+
+        // println!("{}", b.unwrap());
+
         println!("dd: {:?}", sk.get_mut(&dd));
     }
+
+    // #[derive(Debug)]
+    // struct Class {
+    //     a :String,
+    // }
+
+    // impl Class {
+    //     fn get_a(&mut self) -> &mut String {
+    //         &mut self.a
+    //     }
+    // }
+
+    // #[test]
+    // fn t_class() {
+    //     let mut cl = Class{a: "a".to_string()};
+    //     let a = cl.get_a();
+    //     let b = cl.get_a();
+
+    //     *b = "a1".to_string();
+    //     *a = "a2".to_string();
+
+    //     println!("{}", b);
+
+    // }
 }
